2077679747a60211107d5f9cdf3878480adacead
HalaHabboub <22110029@htu.edu.jo>
2025-12-15 15:30:40 +0300

    done css for navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
+// Assuming the CSS file is saved as Navbar.css in the same directory
+
+import './css/Navbar.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from '../Pages/Home.jsx';
 import NotFound from '../Pages/NotFound.jsx';
 import Books from '../Pages/Books.jsx';
 
 
-
 export default function Navbar() {
     return (
-
         <nav>
-            <Link to="/">Home</Link> |{" "}
+            <Link to="/">Home</Link>
+            {/* Using a span for the separator allows us to style it specifically in CSS */}
+            <span className="separator">|</span>
             <Link to="/books">Books</Link>
-
         </nav>
-
-
-    )
+    );
 }
